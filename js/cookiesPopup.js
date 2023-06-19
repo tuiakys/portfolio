10509b53d23c240ea1f2653a4f5af45be342a622
Tuiakys Cunha <tuiakys@gmail.com>
2023-06-18 22:53:04 -0400

    Portfolio v0.1.1.1
    - Nova tentativa de fazer os cookies funcionarem e desfuncionarem com o aceite ou recusa do usuário;

--- js/cookiesPopup.js
+++ js/cookiesPopup.js
@@ -1,37 +1,30 @@
-if (localStorage.getItem('cookie') != null) {
-    document.getElementById('cookies').classList.remove('showCookies');
-}
-
 if (localStorage.getItem('cookie') == null) {
     gtag('consent', 'default', {
         'ad_storage': 'denied'
     });
+} else {
+    document.getElementById('cookies').classList.remove('showCookies');
 }
 
 if (localStorage.getItem('cookie') == 'aceito') {
     gtag('consent', 'update', {
         'ad_storage': 'granted'
     });
-}
 
-if (localStorage.getItem('cookie') == 'recusado') {
+} else {
     gtag('consent', 'default', {
         'ad_storage': 'denied'
     });
 }
 
 document.getElementById('cookieAceito').addEventListener('click', function () {
-    gtag('consent', 'update', {
-        'ad_storage': 'granted'
-    });
     localStorage.setItem('cookie', 'aceito');
     document.getElementById('cookies').classList.remove('showCookies');
+    location.reload();
 });
 
 document.getElementById('cookieRecusado').addEventListener('click', function () {
-    gtag('consent', 'default', {
-        'ad_storage': 'denied'
-    });
     localStorage.setItem('cookie', 'recusado');
     document.getElementById('cookies').classList.remove('showCookies');
+    location.reload();
 });
