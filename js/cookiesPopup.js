1b7a33396319fe90c3ca15d93def2b647a75befd
Tuiakys Cunha <tuiakys@gmail.com>
2023-06-18 21:45:49 -0400

    Portfolio v0.1.6.3
    - Ajuste no script do cookie, agora sim o aviso é consistente e vai armazenar os cookies corretamente (espero);

--- js/cookiesPopup.js
+++ js/cookiesPopup.js
@@ -1,19 +1,19 @@
-if (localStorage.getItem("cookie") != null) {
+if (localStorage.getItem('cookie') != null) {
     document.getElementById('cookies').classList.remove('showCookies');
 }
 
-document.getElementById('cookieAceito').addEventListener("click", function () {
+document.getElementById('cookieAceito').addEventListener('click', function () {
     gtag('consent', 'update', {
         'ad_storage': 'granted'
     });
-    localStorage.setItem("cookie", "aceito");
+    localStorage.setItem('cookie', 'aceito');
     document.getElementById('cookies').classList.remove('showCookies');
 });
 
-document.getElementById('cookieRecusado').addEventListener("click", function () {
+document.getElementById('cookieRecusado').addEventListener('click', function () {
     gtag('consent', 'default', {
         'ad_storage': 'denied'
     });
-    localStorage.setItem("cookie", "recusado");
+    localStorage.setItem('cookie', 'recusado');
     document.getElementById('cookies').classList.remove('showCookies');
 });
--- js/restartCookies.js
+++ js/restartCookies.js
@@ -2,6 +2,6 @@ document.getElementById('restartCookies').addEventListener("click", function () 
     gtag('consent', 'default', {
         'ad_storage': 'denied'
     });
-    localStorage.setItem("cookie", null);
+    localStorage.removeItem('cookie');
     document.getElementById('cookies').classList.add('showCookies');
 });
