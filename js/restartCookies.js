bf92fa29d478ade95542828f9412e711486e6ac3
Tuiakys Cunha <tuiakys@gmail.com>
2023-06-23 17:00:28 -0400

    Portfolio v0.1.1.5
    - Várias correções no script dos Cookies, estilo e restart;
    - Correção no conteúdo geral e título das páginas;

--- js/restartCookies.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('restartCookies').addEventListener("click", function () {
-    gtag('consent', 'default', {
-        'ad_storage': 'denied'
-    });
-    localStorage.removeItem('cookie');
-    document.getElementById('cookies').classList.add('showCookies');
-});
